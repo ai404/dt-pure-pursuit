d55f2af13123c133895e8ffddcadccc34acec489
ai404 <khalifa.abde@outlook.com>
2019-10-16 20:36:31 -0400

    another push

--- packages/pure-pursuit/scripts/lane_controller_node.py
+++ packages/pure-pursuit/scripts/lane_controller_node.py
@@ -92,7 +92,7 @@ class lane_controller(object):
                 """
             elif len(segments[1]["x"])>0:
                 # use yellow points
-                self.process_segments(segments[1] , offset=-0.1, name="yellow")
+                self.process_segments(segments[1] , offset=-0.3, name="yellow") #-.1
             elif len(segments[0]["x"])>0:
                 # use white points
                 #self.process_segments(segments[0] , offset=0.45, name="white")#without scaling
