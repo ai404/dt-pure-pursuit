62c64716ae64df83124b9823e2103c3f067da80f
ai404 <khalifa.abde@outlook.com>
2019-10-16 20:13:26 -0400

    another push

--- build_push.sh
+++ build_push.sh
@@ -2,3 +2,6 @@ git add .
 git commit -am "another push"
 git push
 dts devel build --push -u ai404
+
+docker -H autobot pull ai404/dt-pure-pursuit:v1-arm32v7
+dts duckiebot demo --demo_name lane_follow --package_name pure-pursuit --duckiebot_name autobot --image ai404/dt-pure-pursuit:v1-arm32v7
